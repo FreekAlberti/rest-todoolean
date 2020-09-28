312701b6b2d8898e42553033144308fd0ff35cae
FreekAlberti <federicoalberi96@gmail.com>
2020-09-28 16:18:27 +0200

    chiamata e render

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-  alert();
+  $.ajax(
+    {
+      "url": "http://157.230.17.132:3002/todos",
+      "method": "GET",
+      "success": function (data) {
+        render(data);
+      },
+      "error": function (richiesta, stato, errori) {
+        console.log(errori);
+      }
+    }
+  );
 });
+
+function render(data) {
+  var source = $("#entry-template").html();
+  var template = Handlebars.compile(source);
+  var context = {
+    "data" : data
+  }
+  var html = template(context);
+  $("#lista").append(html);
+}
